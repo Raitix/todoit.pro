cbc5dc854d1e94febadf3af3c07ab1a5d9f137aa
Raitis Ozols <raioz@inbox.lv>
2016-04-28 22:01:32 +0300

    link to git repo

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <html>
-<head></head>
+<head>todoit.pro</head>
 <body>
-<h1>v0 (28.04.2016)</h1>
-<h2>To Do</h2>
-<ul>
-  <li>Internal user registration;</li>
-    <li>Login existence is checked;</li>
-  <li>User registration by Facebook and association to internal user;</li>
-  <li>Voting system;</li>
-  <li>User can create a new task pool by defining the name (it is the same as route) and define visibility;</li>
-  <li>Tabs (task pools): "My", "All", "Main";</li>
-  <li>Configure e-mails (admin@todoit.pro, ...?);</li>
-  <li>Implement Google Analystics;</li>
-  <li>SEO;</li>
-</ul>
-<h2>Minimum viable version</h2>
-<ul>
-<li>Has git repo;</li>
-<li>Oxygen;</li>
-<li>Bootstrap, sb_admin2;</li>
-<li>Login by typing user name (can be not unique);</li>
-<li>User can add/remove+start/finish/cancel a task;</li>
-<li>A new task is created in status "To Do". Other statuses are "In progress", "Canceled", "Done";</li>
-</ul>
+    <h1>v0 (28.04.2016)</h1>
+    <h2>Minimum viable version</h2>
+    <ul>
+        <li>Oxygen;</li>
+        <li>Bootstrap, sb_admin2;</li>
+        <li>Internal user registration (+login existence check)</li>
+        <li>User can add/remove+start/finish/cancel a task;</li>
+        <li>A new task is created in status "To Do". Other statuses are "In progress", "Canceled", "Done";</li>
+    </ul>
+    <h2>To Do</h2>
+    <ul>
+        <li>User registration by Facebook and association to internal user;</li>
+        <li>Voting system: 1) vote up or down a task; 2) vote + or - for a solution of a task;</li>
+        <li>User can create a new task pool by defining the name (used as route) and define accessability;</li>
+        <li>Tabs (task pools): "todoit.pro", "All", "My";</li>
+        <li>Configure e-mails (admin@todoit.pro);</li>
+        <li>Implement Google Analystics;</li>
+        <li>SEO;</li>
+    </ul>
+    <h2>Done</h2>
+    <ul>
+        <li>Git repository created - https://github.com/Raitix/todoit.pro .</li>
+    </ul>
 </body>
 </html>

67c5804366f61de2ed35e2a75e6e69106f901c73
Raitis Ozols <raioz@inbox.lv>
2016-04-28 21:33:33 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head></head>
+<body>
+<h1>v0 (28.04.2016)</h1>
+<h2>To Do</h2>
+<ul>
+  <li>Internal user registration;</li>
+    <li>Login existence is checked;</li>
+  <li>User registration by Facebook and association to internal user;</li>
+  <li>Voting system;</li>
+  <li>User can create a new task pool by defining the name (it is the same as route) and define visibility;</li>
+  <li>Tabs (task pools): "My", "All", "Main";</li>
+  <li>Configure e-mails (admin@todoit.pro, ...?);</li>
+  <li>Implement Google Analystics;</li>
+  <li>SEO;</li>
+</ul>
+<h2>Minimum viable version</h2>
+<ul>
+<li>Has git repo;</li>
+<li>Oxygen;</li>
+<li>Bootstrap, sb_admin2;</li>
+<li>Login by typing user name (can be not unique);</li>
+<li>User can add/remove+start/finish/cancel a task;</li>
+<li>A new task is created in status "To Do". Other statuses are "In progress", "Canceled", "Done";</li>
+</ul>
+</body>
+</html>

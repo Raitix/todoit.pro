37a32f60b4a6e7ec08da8bd6b918b60c92e89f96
Raitis Ozols <raioz@inbox.lv>
2016-05-08 02:13:50 +0300

    created task status label

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -15,87 +15,91 @@
     <!-- Bootstrap Core CSS -->
     <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- sb_admin2 CSS -->
+    <link href="/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/dist/css/main.css" rel="stylesheet">
+
     <!-- Custom Fonts -->
     <link href="/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
 
-    <div id="wrapper">
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel-body">
-                        <p>
-                            <div id="nestable-menu">
-                                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#addTodoModal">Add To Do</button>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel-body">
+                <p>
+                    <div id="nestable-menu">
+                        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#addTodoModal">Add To Do</button>
+                    </div>
+
+                    <!-- Modal -->
+                        <div class="modal fade" id="addTodoModal" tabindex="-1" role="dialog" aria-labelledby="addTodoModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <form method="POST" action="/create-todo">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="addTodoModal">Create new To Do</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label>Title</label>
+                                                <input class="form-control" name="title">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Text</label>
+                                                <textarea class="form-control" rows="3" name="text"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <!-- /.modal -->
+
+                </p>
+
+                <!-- BEGIN status todo -->
+
+                <div class="col-md-12 panel-group" id="panel-group-1">
+
+                        @for ($i = 0; $i < count($todos); $i++)
+
+                            <div class="panel {{ $todos[$i]->getStatusData()['panel-style'] }}" id="panel-{{ $todos[$i]->id }}">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a data-toggle="collapse" data-parent="#panel-group-1" href="#collapse-{{ $todos[$i]->id }}">
+                                            <i class="fa @if ($i === 0) fa-chevron-up @else fa-chevron-down @endif fa-fw"></i>
+                                            {{ $todos[$i]->title }}
+                                        </a>
+                                        <em class="taskLabel pull-right">{{ $todos[$i]->getStatusData()['title'] }}</em>
+                                    </h4>
+                                </div>
+                                <div id="collapse-{{ $todos[$i]->id }}" class="panel-collapse collapse @if ($i === 0) in @endif">
+                                    <div class="panel-body">{!! $todos[$i]->text !!}</div>
+                                </div>
                             </div>
 
-                            <!-- Modal -->
-                                <div class="modal fade" id="addTodoModal" tabindex="-1" role="dialog" aria-labelledby="addTodoModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <form method="POST" action="/create-todo">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                    <h4 class="modal-title" id="addTodoModal">Create new To Do</h4>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="form-group">
-                                                        <label>Title</label>
-                                                        <input class="form-control" name="title">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label>Text</label>
-                                                        <textarea class="form-control" rows="3" name="text"></textarea>
-                                                    </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            <!-- /.modal -->
+                        @endfor
 
-                        </p>
-
-                        <!-- BEGIN status todo -->
-
-                        <div class="col-md-12 panel-group" id="panel-group-1">
-
-                                @for ($i = 0; $i < count($todos); $i++)
-
-                                    <div class="panel {{ $todos[$i]->getStatusData()['panel-style'] }}" id="panel-{{ $todos[$i]->id }}">
-                                        <div class="panel-heading">
-                                            <h4 class="panel-title">
-                                                <a data-toggle="collapse" data-parent="#panel-group-1" href="#collapse-{{ $todos[$i]->id }}">
-                                                    <i class="fa @if ($i === 0) fa-chevron-up @else fa-chevron-down @endif fa-fw"></i>
-                                                    {{ $todos[$i]->title }}
-                                                </a>
-                                            </h4>
-                                        </div>
-                                        <div id="collapse-{{ $todos[$i]->id }}" class="panel-collapse collapse @if ($i === 0) in @endif">
-                                            <div class="panel-body">{!! $todos[$i]->text !!}</div>
-                                        </div>
-                                    </div>
-
-                                @endfor
-
-                        </div>
-
-                        <!-- END status todo -->
-                        
-                    </div>
                 </div>
+
+                <!-- END status todo -->
+                
             </div>
         </div>
     </div>
 
     <script src="/bower_components/jquery/dist/jquery.min.js"></script>    
     <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/dist/js/sb-admin-2.js"></script>
 
     <script>
         $(document).ready(function()
